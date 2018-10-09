0833dead6f1ab1c5e40fb8a9e2faa62001a977cf
Haiyang Zhao <haiyang.zhao@nokia.com>
2018-10-09 15:25:39 +0800

    collect static files

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,6 +1,5 @@
 ﻿#!/bin/bash
-# Don't collect static files
-#python manage.py collectstatic --noinput &&
+python manage.py collectstatic --noinput &&
 python manage.py makemigrations
 python manage.py migrate
 gunicorn --env DJANGO_SETTINGS_MODULE=dashboard.settings dashboard.wsgi -w 2 -b :8000
