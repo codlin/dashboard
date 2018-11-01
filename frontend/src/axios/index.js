0f331593c5c84fbeaaf85a60109b91323d386c48
Haiyang Zhao <haiyang.zhao@nokia.com>
2018-11-01 18:07:57 +0800

    update

--- frontend/src/axios/index.js
+++ frontend/src/axios/index.js
@@ -1,34 +1,35 @@
 import axios from 'axios'
 
 // 全局状态控制引入
-import store from '../store/store'
+import store from '../store/index'
 
 // http request interceptor
-// axios.interceptors.request.use(
-//   // config => {
-//   //   if (store.state.userInfo.token) {
-//   //     // 判断是否存在token，如果存在的话，则每个http header都加上token
-//   //     config.headers.Authorization = `JWT ${store.state.userInfo.token}`
-//   //   }
-//   //   return config
-//   // },
-//   // err => {
-//   //   return Promise.reject(err)
-//   // }
-// )
+axios.interceptors.request.use(
+  config => {
+    if (store.state.userInfo.token) {
+      // 判断是否存在token，如果存在的话，则每个http header都加上token
+      config.headers.Authorization = `JWT ${store.state.userInfo.token}`
+    }
+    return config
+  },
+  err => {
+    return Promise.reject(err)
+  }
+)
 
-// // http response interceptor
-// axios.interceptors.response.use(undefined, error => {
-//   let res = error.response
-//   switch (res.status) {
-//     case 401:
-//       console.log('未登录 或者token过期')
-//     case 403:
-//       console.log('您没有该操作权限')
-//     // alert('您没有该操作权限');
-//     case 500:
-//       console.log('服务器错误')
-//     // alert('服务器错误');
-//   }
-//   return Promise.reject(error.response.data) // 返回接口返回的错误信息
-// })
+// http response interceptor
+axios.interceptors.response.use(undefined, error => {
+  let res = error.response
+  switch (res.status) {
+    case 401:
+      console.log('Not login or token expire.')
+      break
+    case 403:
+      console.log('you have not permission.')
+      break
+    case 500:
+      console.log('Server error.')
+      break
+  }
+  return Promise.reject(error.response.data)
+})
